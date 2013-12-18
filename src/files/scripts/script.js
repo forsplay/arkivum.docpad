31fb79b1e1a7502895dd0501cccdb834bc906b6f
Michael Duane Mooring <mikeumus@gmail.com>
2013-12-18 14:14:07 -0500

    Updating DocPad.

--- src/files/scripts/script.js
+++ src/files/scripts/script.js
@@ -1,21 +1,4 @@
 YUI().use('node', 'event', 'event-valuechange', function(Y) {
-	
-	/*
-	function scrolledPast(){
-		Y.one('.arkivum-logo-stamp').hide();
-	}
-	function scrolledTop(){
-		Y.one('.arkivum-logo-stamp').show();
-	}
-
-	function scrollCheck(){
-		Y.on('scroll', function(){
-			document.documentElement.scrollTop || document.body.scrollTop < 88 ?
-				scrolledTop()
-			:   scrolledPast();
-		});
-	}
-	*/
 
 	// ShareThis
 	Y.all('#shareThisLink #shareIcon').on('click', function(){
